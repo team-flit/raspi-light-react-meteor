273dc3d03521c38c3fc8bef786794843d3d7258f
Seokjun Kim <colus001@me.com>
2015-10-10 04:20:48 +0900

    Update toolbar

--- client/include/Toolbar.jsx
+++ client/include/Toolbar.jsx
@@ -2,12 +2,7 @@ Toolbar = React.createClass({
   render() {
     return (
       <nav className="navbar navbar-inverse">
-        <a className="navbar-brand" href="#">Raspi Light</a>
-        <ul className="nav nav-pills">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-          </li>
-        </ul>
+        <a className="navbar-brand" href="#">Raspberry Light</a>
       </nav>
     )
   }
