a7de4ac0abaf0cde2238fbfa8791f7a76d049fb2
Seokjun Kim <colus001@me.com>
2015-10-09 15:00:57 +0900

    Update toolbar

--- client/include/Toolbar.jsx
+++ client/include/Toolbar.jsx
@@ -7,12 +7,6 @@ Toolbar = React.createClass({
           <li className="nav-item active">
             <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">About</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Contact</a>
-          </li>
         </ul>
       </nav>
     )
